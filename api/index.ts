4bd6984bc084bb94a2e22359dcdba5f9706653ba
Son Tran <son.tran@enouvo.com>
2024-09-04 13:40:11 +0700

    feat(master): init project

---
+++ api/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const config = {
+  runtime: "edge",
+};
+
+const app = new Hono().basePath("/api");
+
+app.get("/", (c) => {
+  return c.json({ message: "Hello Hono!" });
+});
+
+app.get("/apple-app-site-association", async (c) => {
+  console.log(c.req.header());
+  return c.json({
+    applinks: {
+      apps: [],
+      details: [
+        {
+          appID: "PBJWXJYNVL.au.com.acciona.linkedsite.dev",
+          paths: ["NOT /_/*", "/*"],
+        },
+      ],
+    },
+    webcredentials: {
+      apps: ["PBJWXJYNVL.au.com.acciona.linkedsite.dev"],
+    },
+  });
+});
+
+export default handle(app);
